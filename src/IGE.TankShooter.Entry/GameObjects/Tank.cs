d194894d2b383bf4c947dfa7d8fed1dd163d2812
Peter Serwylo <peter@serwylo.com>
2022-07-17 14:26:58 +1000

    Extract Turret into its own class.
    Has a few immediate advantages:
     - no need to prefix everything with body (BodyTransform vs TurretTransform)
    
    And a few longer term advantages:
     - Different turrets could be purchased which have different behaviour, easier in a separate class.

--- src/IGE.TankShooter.Entry/GameObjects/Tank.cs
+++ src/IGE.TankShooter.Entry/GameObjects/Tank.cs
@@ -1,6 +1,5 @@
 ﻿namespace IGE.TankShooter.Entry.GameObjects;
 using System;
-using System.Diagnostics;
 
 using Core;
 
@@ -15,16 +14,12 @@ using MonoGame.Extended.Sprites;
 
 public class Tank : GameObject
 {
-  private Sprite BodySprite;
-  private Sprite TurretSprite;
+  private Turret Turret;
+  
+  private Sprite Sprite;
+  private Transform2 Transform;
 
-  private Transform2 BodyTransform;
-  private Transform2 TurretTransform;
-
-  private Game1 tankGame;
-
-  private const float MAX_TANK_DIRECTION_CHANGE_RATE = MathF.PI / 3; // Radians per second.
-  private const float MAX_TURRET_ROTATION_SPEED = 5f; // degrees per seconds
+  private const float MAX_DIRECTION_CHANGE_RATE = MathF.PI / 3; // Radians per second.
   public const float ACCELERATION = 5.0f; // Units per second.
   public const float MIN_SPEED = 0.0f; // Units per second.
   public const float MAX_SPEED = 10.0f; // Units per second.
@@ -34,13 +29,13 @@ public class Tank : GameObject
 
   public Tank(Game1 tankGame, Point2 initialPosition)
   {
-    this.tankGame = tankGame;
     this.initialPosition = initialPosition;
+    this.Turret = new Turret(tankGame, this);
   }
 
-  public Vector2 CurrentPosition => BodyTransform.Position;
+  public Vector2 CurrentPosition => Transform.Position;
   public float CurrentSpeed => this.velocity.GetScaler().Length();
-  public float CurrentTurretAngle => this.TurretTransform.Rotation;
+  public float CurrentTurretAngle => this.Turret.CurrentAngle;
 
   public override void Initialize()
   {
@@ -53,77 +48,28 @@ public class Tank : GameObject
 
   public override void LoadContent(ContentManager content)
   {
-    var bodyTexture = content.Load<Texture2D>("tankBody_red_outline");
-    var turretTexture = content.Load<Texture2D>("tankRed_barrel1_outline");
+    var texture = content.Load<Texture2D>("tankBody_red_outline");
 
     // Calculate scale based on a desired width of 3m.
     // That same scale factor will be used for the height too but we don't specify a desired height, rather just take
     // the height of the texture and scale it using the same ratio we used to get to 3m width.
-    var spriteScale = 3f / bodyTexture.Width;
+    var spriteScale = 3f / texture.Width;
 
-    this.BodySprite = new Sprite(bodyTexture);
-    this.BodyTransform = new Transform2(new Vector2(initialPosition.X, initialPosition.Y), 0.0f, new Vector2(spriteScale));
-    this.BodyTransform.TranformUpdated += BodyTransform_TranformUpdated;
-
-    this.TurretSprite = new Sprite(turretTexture);
-    this.TurretTransform = new Transform2(new Vector2(initialPosition.X, initialPosition.Y), 0.0f, new Vector2(spriteScale));
-    this.TurretTransform.Parent = this.BodyTransform;
-
-  }
-
-  private void BodyTransform_TranformUpdated()
-  {
-    this.TurretTransform.Position = this.BodyTransform.Position -= Vector2.UnitY * 10;
+    this.Sprite = new Sprite(texture);
+    this.Transform = new Transform2(new Vector2(initialPosition.X, initialPosition.Y), 0.0f, new Vector2(spriteScale));
+   
+    this.Turret.LoadContent(content, initialPosition, spriteScale);
   }
 
   public override void Update(GameTime gameTime)
   {
     this.MoveTank(gameTime);
-    RotateTurretTo(tankGame.Camera.ScreenToWorld(new Vector2(Mouse.GetState().X, Mouse.GetState().Y)), gameTime);
-  }
-
-  private void RotateTurretTo(Vector2 target, GameTime gameTime)
-  {
-    var currentAngle = this.TurretTransform.Rotation * 180 / (float)Math.PI;
-    while (currentAngle < 0)
-    {
-      currentAngle += 360;
-    }
-    while (currentAngle > 360)
-    {
-      currentAngle -= 360;
-    }
-
-    // Not sure what math is doing such that we need to rotate a further 180 degrees, but possibly something like
-    // the world coordinate system being upside down compared to what the intuitive way of calculating angles would
-    // expect?
-    var targetAngle = (target - CurrentPosition).ToAngle() * 180 / (float)Math.PI + 180;
-    while (targetAngle < 0)
-    {
-      targetAngle += 360;
-    }
-    while (targetAngle > 360)
-    {
-      targetAngle -= 360;
-    }
-
-    float toRotate = targetAngle - currentAngle;
-    if (toRotate > 180)
-    {
-      toRotate = -(360 - toRotate);
-    }
-
-    if (toRotate < -180)
-    {
-      toRotate = 360 + toRotate;
-    }
-
-    this.TurretTransform.Rotation = (currentAngle + toRotate * MAX_TURRET_ROTATION_SPEED * gameTime.GetElapsedSeconds()) * ((float)Math.PI / 180);
+    this.Turret.Update(gameTime);
   }
 
   private void RotateTankBodyTo(GameTime gameTime)
   {
-    this.BodyTransform.Rotation = this.velocity.Direction.ToAngle();
+    this.Transform.Rotation = this.velocity.Direction.ToAngle();
   }
 
   private void UpdateTankDirection(GameTime gameTime)
@@ -131,7 +77,7 @@ public class Tank : GameObject
     var deltaTime = gameTime.GetElapsedSeconds();
 
     this.velocity.Direction =
-      Vector2.Lerp(this.velocity.Direction, this.velocity.TargetDirection, deltaTime * MAX_TANK_DIRECTION_CHANGE_RATE);
+      Vector2.Lerp(this.velocity.Direction, this.velocity.TargetDirection, deltaTime * MAX_DIRECTION_CHANGE_RATE);
   }
 
   private void MoveTank(GameTime gameTime)
@@ -165,13 +111,91 @@ public class Tank : GameObject
     RotateTankBodyTo(gameTime);
     var scaler = this.velocity.GetScaler();
 
-    this.BodyTransform.Position += scaler * deltaTime;
-    this.TurretTransform.Position += scaler * deltaTime;
+    this.Transform.Position += scaler * deltaTime;
   }
 
   public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
   {
-    spriteBatch.Draw(BodySprite, BodyTransform);
-    spriteBatch.Draw(TurretSprite, TurretTransform);
+    spriteBatch.Draw(Sprite, Transform);
+    this.Turret.Draw(gameTime, spriteBatch);
+  }
+}
+
+class Turret : GameObject
+{
+  
+  private const float MAX_ROTATION_SPEED = 5f; // degrees per seconds
+ 
+  private Game1 tankGame;
+  private Tank tank;
+  private Sprite Sprite;
+  private Transform2 Transform;
+
+  public Turret(Game1 tankGame, Tank tank)
+  {
+    this.tankGame = tankGame;
+    this.tank = tank;
+  }
+
+  public float CurrentAngle => Transform.Rotation;
+
+  public void LoadContent(ContentManager content, Point2 initialPosition, float spriteScale)
+  {
+    var texture = content.Load<Texture2D>("tankRed_barrel1_outline");
+
+    this.Sprite = new Sprite(texture);
+    this.Sprite.Origin = new Vector2(texture.Width / 2f, texture.Height / 4f);
+    this.Transform = new Transform2(new Vector2(initialPosition.X, initialPosition.Y), 0.0f, new Vector2(spriteScale, spriteScale));
+  }
+
+  public override void Update(GameTime gameTime)
+  {
+    var target = tankGame.Camera.ScreenToWorld(new Vector2(Mouse.GetState().X, Mouse.GetState().Y));
+    this.RotateTurretTo(target, gameTime);
+    this.Transform.Position = tank.CurrentPosition;
+  }
+
+  private void RotateTurretTo(Vector2 target, GameTime gameTime)
+  {
+    var currentAngle = this.Transform.Rotation * 180 / (float)Math.PI;
+    while (currentAngle < 0)
+    {
+      currentAngle += 360;
+    }
+    while (currentAngle > 360)
+    {
+      currentAngle -= 360;
+    }
+
+    // Not sure what math is doing such that we need to rotate a further 180 degrees, but possibly something like
+    // the world coordinate system being upside down compared to what the intuitive way of calculating angles would
+    // expect?
+    var targetAngle = (target - this.tank.CurrentPosition).ToAngle() * 180 / (float)Math.PI + 180;
+    while (targetAngle < 0)
+    {
+      targetAngle += 360;
+    }
+    while (targetAngle > 360)
+    {
+      targetAngle -= 360;
+    }
+
+    float toRotate = targetAngle - currentAngle;
+    if (toRotate > 180)
+    {
+      toRotate = -(360 - toRotate);
+    }
+
+    if (toRotate < -180)
+    {
+      toRotate = 360 + toRotate;
+    }
+
+    this.Transform.Rotation = (currentAngle + toRotate * MAX_ROTATION_SPEED * gameTime.GetElapsedSeconds()) * ((float)Math.PI / 180);
+  }
+
+  public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+  {
+    spriteBatch.Draw(Sprite, Transform);
   }
 }
